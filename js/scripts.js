6be876e0d5e62eed54fa19b56e39fcc8db3dd47e
Ewelina <ewelkarewelka@gazeta.pl>
2017-11-13 12:03:47 +0100

    new repo started

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
+
 // scripts.js
 
 var name = prompt('Enter your name');
+
+// comment 1 
 alert('Hello, ' + name);
+
+// comment 2
 console.log('Hello, ' + name);
+
+//comment 3
+var firstSentence = 'I like a song that is called "Only you".'
